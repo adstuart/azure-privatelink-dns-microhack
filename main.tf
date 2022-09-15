6e07f3982c48182c40c5e1ea0c9deb422d80fee5
Adam Stuart <51229242+adstuart@users.noreply.github.com>
2022-09-15 07:22:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"      version = "2.92.0"
+    }
+  }
+}
+
 provider "azurerm" {
   version = "=2.0.0"
   features {}
