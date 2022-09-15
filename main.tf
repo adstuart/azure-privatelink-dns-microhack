ab415bb0656ed74cb92e6769517bb90b2067b059
Adam Stuart <51229242+adstuart@users.noreply.github.com>
2022-09-15 07:32:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"      version = "2.92.0"
+      source = "hashicorp/azurerm"     
+	  version = "2.92.0"
     }
   }
 }
 
 provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+#######################################################################
+## Create Resource Group
+#######################################################################
 
 resource "azurerm_resource_group" "privatelink-dns-microhack-rg" {
   name     = "privatelink-dns-microhack-rg"
@@ -17,5 +25,4 @@ resource "azurerm_resource_group" "privatelink-dns-microhack-rg" {
     deployment  = "terraform"
     microhack   = "privatelink-dns"
   }
-}
 }
